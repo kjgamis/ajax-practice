85ddcdac234f6e021bb11d009787b66207463d5a
Karen <kjgamis@live.ca>
2018-06-19 17:04:34 -0400

    save coordinates in hashes

--- scoop-api/script.js
+++ scoop-api/script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // var pearsonLat =
+  var pearson = {'lat': 43.6777, 'long': -79.6248}
+  var cnTower = {'lat': 43.6426, 'long': -79.3871}
+
 
   $.ajax({
-    url:       'http://www.scoopfare.com/price/show?sl=43.6777&slon=-79.6248&el=43.6426&elon=-79.3871',
+    url:       'http://www.scoopfare.com/price/show?sl=' + pearson['lat'] + '&slon=' + pearson['long'] + '&el=' + cnTower['lat'] + '&elon=' + cnTower['long'] + '',
     method:    'GET',
     dataType:  'json'
   }).done( function(responseData) {
