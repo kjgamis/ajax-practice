c67b7efeb9dd3c0854e065293a33a8a74512672d
Karen <kjgamis@live.ca>
2018-06-19 16:51:12 -0400

    able to fetch data successfully

--- scoop-api/script.js
+++ scoop-api/script.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  
+  // var pearsonLat =
+
+  $.ajax({
+    url:       'http://www.scoopfare.com/price/show?sl=43.6777&slon=-79.6248&el=43.6426&elon=-79.3871',
+    method:    'GET',
+    dataType:  'json'
+  }).done( function(responseData) {
+    console.log(responseData);
+  })
 
 });
