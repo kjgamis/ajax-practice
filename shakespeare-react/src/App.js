a08819ea25ce3414bbbfce53d31a56d10e4a69e7
Karen <kjgamis@live.ca>
2018-06-20 12:49:19 -0400

    get request to shakespeare api working

--- shakespeare-react/src/App.js
+++ shakespeare-react/src/App.js
@@ -11,13 +11,31 @@ class App extends Component {
   }
 
   componentDidMount() {
-    
+    // save a reference to `this` because the value of `this` will change
+    // inside the different callback functions.
+    var base = this
+
+    // fetch a componentDidMount
+    let poemApi = 'http://ShakeItSpeare.com/api/poem';
+    fetch(poemApi)
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        base.setState({
+          shakeSpeare: json.poem
+        })
+      }).catch((ex) => {
+        console.log('An error occured while parsing.', ex)
+      })
   }
 
   render() {
+    let poetry = this.state.shakeSpeare;
+    
     return (
       <div>
         <h1>My favourite Shakespeare poem:</h1>
+        {poetry}
       </div>
     );
   }
